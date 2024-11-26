8f3b913e76be5eee9611e0fe873f9ebee5746958
José Baur <joseluisbaur@gmail.com>
2024-11-26 14:37:29 -0600

    Changes in coffeshop

--- products/models.py
+++ products/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.TextField(max_length=100, verbose_name='nombre')
-    description = models.TextField(max_length=300, verbose_name='descripcion')
+    description = models.TextField(max_length=200, verbose_name='descripcion')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='precio')
     avaliable = models.BooleanField(default=True, verbose_name='disponible')
-    photo=models.ImageField(upload_to='logos', null=True, blank=True, verbose_name='foto')
+    image=models.ImageField(upload_to='logos', null=True, blank=True, verbose_name='foto')
 
     def __str__(self):
         return self.name
